43cc4dc35daa8f6ee91d989b5528b3b7e7393c6c
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-17 15:02:11 +0000

    Automatically backed up by Learn

--- spec/03_cli_spec.rb
+++ spec/03_cli_spec.rb
@@ -1,8 +1,6 @@
 require_relative "../lib/move.rb"
 
 
-board=[" ", " ", " "," ", " ", " "," ", " "," "]
-
 describe './bin/move executing a CLI Application' do
   it 'defines a board variable' do
     allow($stdout).to receive(:puts)
